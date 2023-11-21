b4f72f3200019e45070dfcbb2d6a27ca8574cf42
Bob Webb <bob.wbb@gmail.com>
2023-11-21 13:54:05 +0000

    initial commit, install @magic-ext/* packages

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import algorand from "@magic-ext/algorand"
+import aptos from "@magic-ext/aptos"
+import auth from "@magic-ext/auth"
+import bitcoin from "@magic-ext/bitcoin"
+import conflux from "@magic-ext/conflux"
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  <div>
+    placeholder
+  </div>
+`
